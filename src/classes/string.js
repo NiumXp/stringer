0c8b94f120d8ae1796e10d8ee8870bfbf061bfce
deivinni <deivinnimds3656@gmail.com>
2023-03-22 21:41:21 -0300

    config: `eslint`

--- src/classes/string.js
+++ src/classes/string.js
@@ -1,28 +1,12 @@
 export default class UpString extends String {
   /**
-   * @constructor
-   * @param {string} string 
-   */
-  constructor (string) {
-    super(string);
-  }
-
-  /**
-   * @param {boolean} lower 
+   * @param {boolean} lower
    * @example
    * console.log(new UpString("hello!").toFirstUpperCase());
    *  // Hello!
    * @returns
    */
-  toFirstUpperCase (lower) {
+  toFirstUpperCase(lower) {
     return this[0].toUpperCase() + (lower ? this.toLowerCase() : this).slice(1);
-  }
-
-  /**
-   * @param {string[][]} replacers
-   * @returns
-   */
-  replaceAll(replacers) {
-
   }
 }
