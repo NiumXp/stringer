a9356c329383cb8ba84cdc6409dcf828f2ef9cf2
deivinni <deivinnimds3656@gmail.com>
2023-03-22 21:47:32 -0300

    move `tests`

--- src/classes/string.js
+++ src/classes/string.js
@@ -2,9 +2,10 @@ export default class UpString extends String {
   /**
    * @param {boolean} lower
    * @example
-   * console.log(new UpString("hello!").toFirstUpperCase());
-   *  // Hello!
-   * @returns
+   * const text = new UpString("hello!");
+   *
+   * console.log(text.toFirstUpperCase()); // Hello!
+   * @returns {string}
    */
   toFirstUpperCase(lower) {
     return this[0].toUpperCase() + (lower ? this.toLowerCase() : this).slice(1);
