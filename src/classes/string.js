3c7378856c7b93775b374cdd2af40022c913ad19
deivinni <deivinnimds3656@gmail.com>
2023-03-22 22:55:53 -0300

    add `replaceArray`, `toString` and `toJSON`

--- src/classes/string.js
+++ src/classes/string.js
@@ -1,13 +1,57 @@
 export default class UpString extends String {
   /**
+   * @param {string} string
+   */
+  constructor(string) {
+    super();
+
+    /**
+     * @type {string}
+     */
+    this.string = string;
+  }
+
+  /**
    * @param {boolean} lower
    * @example
    * const text = new UpString("hello!");
    *
-   * console.log(text.toFirstUpperCase()); // Hello!
-   * @returns {string}
+   * console.log(text.toFirstUpperCase().toString()); // Hello!
+   * @returns {UpString}
    */
   toFirstUpperCase(lower) {
-    return this[0].toUpperCase() + (lower ? this.toLowerCase() : this).slice(1);
+    this.string = this.string[0].toUpperCase()
+      + (lower ? this.string.toLowerCase() : this.string).slice(1);
+
+    return this;
+  }
+
+  /**
+   * @param {Array<Array<(RegExp | string), string>>} replacers
+   * @returns {UpString}
+   */
+  replaceArray(replacers) {
+    let text = this.string;
+
+    // eslint-disable-next-line no-restricted-syntax, prefer-const
+    for (let [find, replacer] of replacers) {
+      if (!(find instanceof RegExp)) find = new RegExp(find, 'g');
+
+      text = text.replace(find, replacer);
+    }
+
+    this.string = text;
+
+    return this;
+  }
+
+  toString() {
+    return this.string;
+  }
+
+  toJSON() {
+    return {
+      string: this.string,
+    };
   }
 }
